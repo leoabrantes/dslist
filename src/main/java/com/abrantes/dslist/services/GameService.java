b8f7f5238818cac9e84a36d5235cf28e85cf6312
Leo Abrantes <leoabrantesmelo@gmail.com>
2023-05-12 17:31:37 -0300

    Finding by Id

--- src/main/java/com/abrantes/dslist/services/GameService.java
+++ src/main/java/com/abrantes/dslist/services/GameService.java
@@ -5,7 +5,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.abrantes.dslist.dto.GameDTO;
 import com.abrantes.dslist.dto.GameMinDTO;
 import com.abrantes.dslist.entities.Game;
 import com.abrantes.dslist.repositories.GameRepository;
@@ -16,9 +18,19 @@ public class GameService {
 	@Autowired
 	private GameRepository  gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+		
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).collect(Collectors.toList());
 	}
+	
+	
 
 }
